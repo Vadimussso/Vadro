8ebfa6c4eb4fd94aace8fa021cee94c83f8a3757
Vadimussso <auto.rus14@gmail.com>
2024-07-14 21:56:08 +0400

    add get 1 item endpoint

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
@@ -21,4 +21,16 @@ def read_ads(db=Depends(get_db)):
         cursor.execute("SELECT * FROM ads")
         items = cursor.fetchall()
         return items
+
+
+@app.get("/ads/{item_id}")
+def read_ad(item_id, db=Depends(get_db)):
+    with db.cursor() as cursor:
+        cursor.execute("SELECT * FROM ads WHERE id = %s", [item_id])
+        item = cursor.fetchone()
+        if item is None:
+            raise HTTPException(status_code=404, detail="Item not found")
+        return item
+
+
 
