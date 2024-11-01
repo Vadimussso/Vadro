8b82868d85b56643df95cf1f7215a1e350f26548
Vadimussso <auto.rus14@gmail.com>
2024-11-01 23:24:03 +0400

    added protocols and services

--- main.py
+++ main.py
@@ -166,40 +166,50 @@ class UserRequiredError(ValueError):
         super().__init__(message)
 
 
-def insert_ad(user_id: int, ad: Ad, db) -> int:
-    # if id exist, ad applying is carry on
-    with db.cursor() as cursor:
-        # attempt to apply the ad:
-        cursor.execute(
-            """
-            INSERT INTO ads (
-                created_at, vin, vrc, license_plate, brand, model, mileage, 
-                engine_capacity, price, description, city, phone, posted_at, author_id
-            ) VALUES (
-                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
-            ) RETURNING id
-            """,
-            (
-                datetime.now(), ad.vin, ad.vrc, ad.license_plate, ad.brand,
-                ad.model, ad.mileage, ad.engine_capacity, ad.price,
-                ad.description, ad.city, ad.phone, datetime.now(), user_id
-            )
-        )
-        db.commit()
-
-        return cursor.fetchone()['id']
-
-
-class AdService():
+class AdRepo:
     def __init__(self, db=Depends(get_db)):
         self.db = db
+
+    def insert_ad(self, user_id: int, ad: Ad) -> int:
+        # if id exist, ad applying is carry on
+        with self.db.cursor() as cursor:
+            # attempt to apply the ad:
+            cursor.execute(
+                """
+                INSERT INTO ads (
+                    created_at, vin, vrc, license_plate, brand, model, mileage, 
+                    engine_capacity, price, description, city, phone, posted_at, author_id
+                ) VALUES (
+                    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+                ) RETURNING id
+                """,
+                (
+                    datetime.now(), ad.vin, ad.vrc, ad.license_plate, ad.brand,
+                    ad.model, ad.mileage, ad.engine_capacity, ad.price,
+                    ad.description, ad.city, ad.phone, datetime.now(), user_id
+                )
+            )
+            self.db.commit()
+
+            return cursor.fetchone()['id']
+
+
+class AdRepoProtocol(Protocol):
+    def insert_ad(self, user_id: int, ad: Ad) -> int:
+        pass
+
+
+class AdService:
+
+    def __init__(self, ad_repo: AdRepoProtocol = Depends(AdRepo)):
+        self.ad_repo = ad_repo
 
     def add_ad(self, user_id: int | None, ad: Ad) -> int:
 
         if user_id is None:
             raise UserRequiredError()
 
-        return insert_ad(user_id, ad, self.db)
+        return self.ad_repo.insert_ad(user_id, ad)
 
 
 class AdServiceProtocol(Protocol):
